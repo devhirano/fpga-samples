e750c9bda0156a6c9ce043976795f242e57e64a4
devhirano <devhirano@gmail.com; export GIT_COMMITER_NAME=devhirano; export GIT_COMMITER_EMAIL=devhirano@gmail.com>
2018-01-29 00:20:06 +0900

    delete needless files

--- verilog/README.txt
+++
@@ -1,3 +0,0 @@
-Verilog入門用
-http://cas.eedept.kobe-u.ac.jp/~arai/Verilog/chap4.html
-
--- verilog/hello.v
+++
@@ -1,7 +0,0 @@
-module main;
-  initial 
-    begin
-      $display("Hello, Wolrd");
-      $finish;
-    end
-endmodule
